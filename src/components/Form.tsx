8b1f158894e585fb5b440e13ee512d8090a57588
SrJaggy <jag8210@gmail.com>
2025-07-28 06:31:01 -0500

    First commit

---
+++ src/components/Form.tsx
@@ -0,0 +1,13 @@
+
+
+export const Form = () => {
+  return (
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="category">Categoría:</label>
+            <select className="border border-slate-300 p-2 rounded-lg w-full bg-white" id="category"></select>
+        </div>
+
+    </form>
+  )
+}
